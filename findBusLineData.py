a14b84a3fb0816c046bbc03253ab2d4e3d36e7ae
QiaoEnLiu <137969620+QiaoEnLiu@users.noreply.github.com>
2024-10-02 15:51:39 +0800

    2024 1002 1555

--- findBusLineData.py
+++ findBusLineData.py
@@ -6,18 +6,41 @@ busLinesData = busLineModel.readJson_File()
 BusID = "132路"
 BusStop = "朝陽科技大學"
 
+takeStop = "朝陽科技大學"
+desStop = "逢甲大學(福星路)"
+
 def findBus():
 
     for bus in busLinesData:
         if bus[busLineModel.BusLineID] == BusID:
-            print(bus)
+            print(bus[busLineModel.BusLineID], bus[busLineModel.BusLineName], bus[busLineModel.Com_BusLine], sep = "\n")
+            print(busLineModel.RoundTrip_ob, bus[busLineModel.RoundTrip_ob].split("停靠：")[1].split("、"), sep = ":")
+            print(busLineModel.RoundTrip_ib, bus[busLineModel.RoundTrip_ib].split("停靠：")[1].split("、"), sep = ":")
+            print("--------------")
 
 
-def findStop():
-
+def findStop(stop):
+    stopData = []
     for bus in busLinesData:
-        if BusStop in bus[busLineModel.RoundTrip_ob].split("停靠：")[1].split("、") or BusStop in bus[busLineModel.RoundTrip_ib].split("停靠：")[1].split("、"):
-            print(bus[busLineModel.BusLineID])
+        if stop in bus[busLineModel.RoundTrip_ob].split("停靠：")[1].split("、") or stop in bus[busLineModel.RoundTrip_ib].split("停靠：")[1].split("、"):
+            '''
+            print(bus[busLineModel.BusLineID], bus[busLineModel.BusLineName], bus[busLineModel.Com_BusLine], sep = "\n")
+            print(busLineModel.RoundTrip_ob, bus[busLineModel.RoundTrip_ob].split("停靠：")[1].split("、"), sep = ":")
+            print(busLineModel.RoundTrip_ib, bus[busLineModel.RoundTrip_ib].split("停靠：")[1].split("、"), sep = ":")
+            print("--------------")
+            '''
+
+
+            tempDict = {busLineModel.BusLineID: bus[busLineModel.BusLineID],
+                        busLineModel.RoundTrip_ob: bus[busLineModel.RoundTrip_ob].split("停靠：")[1].split("、"),
+                        busLineModel.RoundTrip_ib: bus[busLineModel.RoundTrip_ib].split("停靠：")[1].split("、")
+                        }
+            stopData.append(tempDict)
+
+    return stopData
 
 # findBus()
-findStop()
+# findStop()
+
+desStopData = findStop(desStop)
+takeStopData = findStop(takeStop)
